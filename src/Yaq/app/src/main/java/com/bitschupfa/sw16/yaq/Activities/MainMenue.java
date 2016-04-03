1e1fc9c89154271cbcbb244afb0185e32473852d
Johannes Kühnel <johannes.kuehnel@student.tugraz.at>
2016-04-04 00:12:38 +0200

    [JK, MW] add initial cast button

--- src/Yaq/app/src/main/java/com/bitschupfa/sw16/yaq/Activities/MainMenue.java
+++ src/Yaq/app/src/main/java/com/bitschupfa/sw16/yaq/Activities/MainMenue.java
@@ -2,16 +2,28 @@ package com.bitschupfa.sw16.yaq.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.bitschupfa.sw16.yaq.R;
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
 
 public class MainMenue extends AppCompatActivity {
+
+    public static MediaRouter mMediaRouter;
+    public static MediaRouteSelector mMediaRouteSelector;
+    public static CastDevice mSelectedDevice;
+    public static MyMediaRouterCallback mMediaRouterCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +31,36 @@ public class MainMenue extends AppCompatActivity {
         setContentView(R.layout.activity_main_menue);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
+        mMediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(CastMediaControlIntent.categoryForCast("5CC4A228"))
+                .build();
+        mMediaRouterCallback = new MyMediaRouterCallback();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_menue, menu);
+        MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+        MediaRouteActionProvider mediaRouteActionProvider =
+                (MediaRouteActionProvider) MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+        mediaRouteActionProvider.setRouteSelector(mMediaRouteSelector);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMediaRouter.addCallback(mMediaRouteSelector, mMediaRouterCallback,
+                MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+    }
+
+    @Override
+    protected void onStop() {
+        mMediaRouter.removeCallback(mMediaRouterCallback);
+        super.onStop();
     }
 
     @Override
@@ -58,5 +93,22 @@ public class MainMenue extends AppCompatActivity {
     public void joinButtonClicked(View view) {
         Intent intent = new Intent(MainMenue.this, Join.class);
         startActivity(intent);
+    }
+
+    private class MyMediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            mSelectedDevice = CastDevice.getFromBundle(info.getExtras());
+            String routeId = info.getId();
+            Log.d(MainMenue.class.getCanonicalName(), "onRouteSelected: " + routeId);
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo info) {
+            //teardown();
+            mSelectedDevice = null;
+            Log.d(MainMenue.class.getCanonicalName(), "onRouteUnselected: ");
+        }
     }
 }
