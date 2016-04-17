7a8c370c272ea5eb1d0b0e0c1c65a4b94c6a419b
Johannes Kühnel <johannes.kuehnel@student.tugraz.at>
2016-04-18 01:03:52 +0200

    [JK, MW] added Chromecast sample functionality

--- src/Yaq/app/src/main/java/com/bitschupfa/sw16/yaq/Activities/MainMenue.java
+++ src/Yaq/app/src/main/java/com/bitschupfa/sw16/yaq/Activities/MainMenue.java
@@ -15,15 +15,37 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.bitschupfa.sw16.yaq.R;
+import com.google.android.gms.cast.ApplicationMetadata;
+import com.google.android.gms.cast.Cast;
 import com.google.android.gms.cast.CastDevice;
 import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import java.io.IOException;
 
 public class MainMenue extends AppCompatActivity {
+
+    public static String TAG = MainMenue.class.getCanonicalName();
 
     public static MediaRouter mMediaRouter;
     public static MediaRouteSelector mMediaRouteSelector;
     public static CastDevice mSelectedDevice;
     public static MyMediaRouterCallback mMediaRouterCallback;
+    public static GoogleApiClient mApiClient;
+
+    public static Cast.Listener mCastClientListener;
+    public static ConnectionCallbacks mConnectionCallbacks;
+    public static ConnectionFailedListener mConnectionFailedListener;
+
+    public static HelloWorldChannel mHelloWorldChannel;
+
+    public static boolean mWaitingForReconnect;
+    public static boolean mApplicationStarted;
+
+    private static String APPLICATION_ID = "5CC4A228";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +56,33 @@ public class MainMenue extends AppCompatActivity {
 
         mMediaRouter = MediaRouter.getInstance(getApplicationContext());
         mMediaRouteSelector = new MediaRouteSelector.Builder()
-                .addControlCategory(CastMediaControlIntent.categoryForCast("5CC4A228"))
-                .build();
+                .addControlCategory(CastMediaControlIntent.categoryForCast(APPLICATION_ID))
+                        .build();
         mMediaRouterCallback = new MyMediaRouterCallback();
+
+        mConnectionCallbacks = new ConnectionCallbacks();
+        mConnectionFailedListener = new ConnectionFailedListener();
+        mCastClientListener = new Cast.Listener() {
+            @Override
+            public void onApplicationStatusChanged() {
+                if (mApiClient != null) {
+                    Log.d(TAG, "onApplicationStatusChanged: "
+                            + Cast.CastApi.getApplicationStatus(mApiClient));
+                }
+            }
+
+            @Override
+            public void onVolumeChanged() {
+                if (mApiClient != null) {
+                    Log.d(TAG, "onVolumeChanged: " + Cast.CastApi.getVolume(mApiClient));
+                }
+            }
+
+            @Override
+            public void onApplicationDisconnected(int errorCode) {
+                //teardown();
+            }
+        };
     }
 
     @Override
@@ -72,12 +118,15 @@ public class MainMenue extends AppCompatActivity {
 
         if (id == R.id.menu_settings) {
             Toast.makeText(this, R.string.not_implemented, Toast.LENGTH_SHORT).show();
+            sendMessage("Settings clicked");
             return true;
         } else if (id == R.id.menu_profile) {
             Toast.makeText(this, R.string.not_implemented, Toast.LENGTH_SHORT).show();
+            sendMessage("Profile clicked");
             return true;
         } else if (id == R.id.menu_manage) {
             Toast.makeText(this, R.string.not_implemented, Toast.LENGTH_SHORT).show();
+            sendMessage("Manage clicked");
             return true;
         }
 
@@ -102,6 +151,17 @@ public class MainMenue extends AppCompatActivity {
             mSelectedDevice = CastDevice.getFromBundle(info.getExtras());
             String routeId = info.getId();
             Log.d(MainMenue.class.getCanonicalName(), "onRouteSelected: " + routeId);
+
+            Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions
+                    .builder(mSelectedDevice, mCastClientListener);
+
+            mApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                    .addApi(Cast.API, apiOptionsBuilder.build())
+                    .addConnectionCallbacks(mConnectionCallbacks)
+                    .addOnConnectionFailedListener(mConnectionFailedListener)
+                    .build();
+
+            mApiClient.connect();
         }
 
         @Override
@@ -111,4 +171,94 @@ public class MainMenue extends AppCompatActivity {
             Log.d(MainMenue.class.getCanonicalName(), "onRouteUnselected: ");
         }
     }
+
+    private class ConnectionCallbacks implements
+            GoogleApiClient.ConnectionCallbacks {
+        @Override
+        public void onConnected(Bundle connectionHint) {
+            if (mWaitingForReconnect) {
+                mWaitingForReconnect = false;
+                //reconnectChannels();
+            } else {
+                try {
+                    Cast.CastApi.launchApplication(mApiClient, APPLICATION_ID, false)
+                            .setResultCallback(
+                                    new ResultCallback<Cast.ApplicationConnectionResult>() {
+                                        @Override
+                                        public void onResult(Cast.ApplicationConnectionResult result) {
+                                            Status status = result.getStatus();
+                                            if (status.isSuccess()) {
+                                                ApplicationMetadata applicationMetadata =
+                                                        result.getApplicationMetadata();
+                                                String sessionId = result.getSessionId();
+                                                String applicationStatus = result.getApplicationStatus();
+                                                boolean wasLaunched = result.getWasLaunched();
+
+                                                mApplicationStarted = true;
+
+                                                mHelloWorldChannel = new HelloWorldChannel();
+                                                try {
+                                                    Cast.CastApi.setMessageReceivedCallbacks(mApiClient,
+                                                            mHelloWorldChannel.getNamespace(),
+                                                            mHelloWorldChannel);
+                                                } catch (IOException e) {
+                                                    Log.e(TAG, "Exception while creating channel", e);
+                                                }
+                                            } else {
+                                                //teardown();
+                                            }
+                                        }
+                                    });
+
+                } catch (Exception e) {
+                    Log.e(MainMenue.class.getCanonicalName(), "Failed to launch application", e);
+                }
+            }
+        }
+
+        @Override
+        public void onConnectionSuspended(int cause) {
+            mWaitingForReconnect = true;
+        }
+    }
+
+    private class ConnectionFailedListener implements
+            GoogleApiClient.OnConnectionFailedListener {
+        @Override
+        public void onConnectionFailed(ConnectionResult result) {
+            //teardown();
+        }
+    }
+
+    class HelloWorldChannel implements Cast.MessageReceivedCallback {
+        public String getNamespace() {
+            return "urn:x-cast:com.google.cast.sample.helloworld";
+        }
+
+        @Override
+        public void onMessageReceived(CastDevice castDevice, String namespace,
+                                      String message) {
+            Log.d(TAG, "onMessageReceived: " + message);
+        }
+    }
+
+    private void sendMessage(String message) {
+        if (mApiClient != null && mHelloWorldChannel != null) {
+            try {
+                Cast.CastApi.sendMessage(mApiClient, mHelloWorldChannel.getNamespace(), message)
+                        .setResultCallback(
+                                new ResultCallback<Status>() {
+                                    @Override
+                                    public void onResult(Status result) {
+                                        if (!result.isSuccess()) {
+                                            Log.e(TAG, "Sending message failed");
+                                        }
+                                    }
+                                });
+            } catch (Exception e) {
+                Log.e(TAG, "Exception while sending message", e);
+            }
+        }
+    }
 }
+
