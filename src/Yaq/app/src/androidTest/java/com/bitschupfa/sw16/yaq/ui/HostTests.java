d9fb7618f202b2afc013d87c090ad017b9ec5855
Thomas Mauerhofer <thomas.mauerhofer@student.tugraz.at>
2016-04-19 11:00:53 +0200

    [MW, TM] Refactor Testcases

--- src/Yaq/app/src/androidTest/java/com/bitschupfa/sw16/yaq/ui/HostTests.java
+++ src/Yaq/app/src/androidTest/java/com/bitschupfa/sw16/yaq/ui/HostTests.java
@@ -1,5 +1,6 @@
 package com.bitschupfa.sw16.yaq.ui;
 
+import android.bluetooth.BluetoothAdapter;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.bitschupfa.sw16.yaq.Activities.Host;
@@ -21,8 +22,16 @@ public class HostTests extends ActivityInstrumentationTestCase2<Host> {
     @Override
     public void setUp() throws Exception {
         solo = new Solo(getInstrumentation(), getActivity());
-    }
 
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            throw new Exception("Bluetooth is not supported.. Please don't use an emulator!");
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                throw new Exception("Bluetooth is not enabled..");
+            }
+        }
+    }
 
     @Override
     public void tearDown() throws Exception {
