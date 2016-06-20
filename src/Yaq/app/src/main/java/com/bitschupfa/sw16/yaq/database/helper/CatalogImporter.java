ec957870c0ca0fefe8927e55252dd696dfbf20dd
Johannes Kühnel <johannes.kuehnel@student.tugraz.at>
2016-06-20 17:57:27 +0200

    [JK, AM] merged question editing

--- src/Yaq/app/src/main/java/com/bitschupfa/sw16/yaq/database/helper/CatalogImporter.java
+++ src/Yaq/app/src/main/java/com/bitschupfa/sw16/yaq/database/helper/CatalogImporter.java
@@ -1,22 +1,15 @@
-package com.bitschupfa.sw16.yaq.activities;
+package com.bitschupfa.sw16.yaq.database.helper;
 
 import android.Manifest;
-import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 
-import com.bitschupfa.sw16.yaq.R;
 import com.bitschupfa.sw16.yaq.database.dao.QuestionCatalogDAO;
 import com.bitschupfa.sw16.yaq.database.dao.TextQuestionDAO;
-import com.bitschupfa.sw16.yaq.database.helper.QuestionQuerier;
 import com.bitschupfa.sw16.yaq.database.object.Answer;
 import com.bitschupfa.sw16.yaq.database.object.QuestionCatalog;
 import com.bitschupfa.sw16.yaq.database.object.TextQuestion;
@@ -29,34 +22,23 @@ import java.io.IOException;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class QuizMaker extends AppCompatActivity {
+public class CatalogImporter {
     int questionCatalogId = 0;
+    private Activity activity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz_maker);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                importFile(view);
-            }
-        });
+    public CatalogImporter(Activity activity) {
+        this.activity = activity;
     }
 
     public  boolean isStoragePermissionGranted() {
         if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            if (activity.checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
                     == PackageManager.PERMISSION_GRANTED) {
                 Log.d("ERROR","Permission is granted");
                 return true;
             } else {
                 Log.d("ERROR","Permission is revoked");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
                 return false;
             }
         }
@@ -81,23 +63,12 @@ public class QuizMaker extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
-            Log.v("Error","Permission: " + permissions[0] + "was " + grantResults[0]);
-            //resume tasks needing this permission
-            importFile(findViewById(R.id.fab));
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    private void importFile(View view) {
+    public void importFile() {
         if(!isStoragePermissionGranted()){
-            Snackbar.make(view, "No permission for reading storage", Snackbar.LENGTH_LONG)
+            Snackbar.make(activity.getCurrentFocus(), "No permission for reading storage", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
         } else {
-            new FileChooser(this).setFileListener(new FileChooser.FileSelectedListener() {
+            new FileChooser(activity).setFileListener(new FileChooser.FileSelectedListener() {
                 @Override
                 public void fileSelected(final File file) {
                     try {
@@ -110,22 +81,6 @@ public class QuizMaker extends AppCompatActivity {
         }
     }
 
-    public static boolean isInteger(String s) {
-        return isInteger(s,10);
-    }
-
-    public static boolean isInteger(String s, int radix) {
-        if(s.isEmpty()) return false;
-        for(int i = 0; i < s.length(); i++) {
-            if(i == 0 && s.charAt(i) == '-') {
-                if(s.length() == 1) return false;
-                else continue;
-            }
-            if(Character.digit(s.charAt(i),radix) < 0) return false;
-        }
-        return true;
-    }
-
     private void readCatalog(BufferedReader reader) throws IOException {
         String questionCatalogName = reader.readLine();
         if (questionCatalogName == null) {
@@ -133,16 +88,20 @@ public class QuizMaker extends AppCompatActivity {
         }
 
         String difficultyString = reader.readLine();
-        if (!isInteger(difficultyString)) {
+
+        int difficulty;
+        try {
+            difficulty = Integer.parseInt(difficultyString);
+        } catch (NumberFormatException e) {
             throw new IOException();
         }
-        int difficulty = Integer.parseInt(difficultyString);
+
         QuestionCatalog questionCatalog1 = new QuestionCatalog(0, difficulty, questionCatalogName, null);
         QuestionCatalogDAO questionCatalogDAO = new QuestionCatalogDAO(questionCatalog1);
-        questionCatalogDAO.insertIntoDatabase(this);
+        questionCatalogDAO.insertIntoDatabase(activity);
         boolean catalogFound = false;
 
-        QuestionQuerier questionQuerier = new QuestionQuerier(this);
+        QuestionQuerier questionQuerier = new QuestionQuerier(activity);
         List<QuestionCatalog> questionCatalogs = questionQuerier.getAllQuestionCatalogsOnlyIdAndName();
         for (QuestionCatalog questionCatalog : questionCatalogs) {
             if (questionCatalog.getName().equals(questionCatalogName)) {
@@ -170,15 +129,15 @@ public class QuizMaker extends AppCompatActivity {
         String answer3ValueString = stringTokenizer.nextToken();
         String answer4ValueString = stringTokenizer.nextToken();
 
-        if (!isInteger(answer1ValueString) || !isInteger(answer2ValueString) ||
-                !isInteger(answer3ValueString) || !isInteger(answer4ValueString)) {
+        int answer1Value, answer2Value, answer3Value, answer4Value;
+        try {
+            answer1Value = Integer.parseInt(answer1ValueString);
+            answer2Value = Integer.parseInt(answer2ValueString);
+            answer3Value = Integer.parseInt(answer3ValueString);
+            answer4Value = Integer.parseInt(answer4ValueString);
+        } catch (NumberFormatException e) {
             throw new IOException();
         }
-
-        int answer1Value = Integer.parseInt(answer1ValueString);
-        int answer2Value = Integer.parseInt(answer2ValueString);
-        int answer3Value = Integer.parseInt(answer3ValueString);
-        int answer4Value = Integer.parseInt(answer4ValueString);
 
         Answer answer1 = new Answer(answer1String, answer1Value);
         Answer answer2 = new Answer(answer2String, answer2Value);
@@ -187,7 +146,7 @@ public class QuizMaker extends AppCompatActivity {
 
         TextQuestion textQuestion = new TextQuestion(0, question, answer1, answer2, answer3, answer4, questionCatalogId);
         TextQuestionDAO textQuestionDAO = new TextQuestionDAO(textQuestion);
-        textQuestionDAO.insertIntoDatabase(this);
+        textQuestionDAO.insertIntoDatabase(activity);
     }
 
 }
